9f533245cc57e01bafbd09b8488f9907c8e82b6a
linasa <lina.b4007@gmail.com>
2021-05-19 23:57:59 +0200

    Décodage César
    Il semblerait que de façon aléatoire il rajoute un 8 ou un B (ou b)  à la fin de la chaine ???
    Mais si je re compile ça passe, jusqu'à la prochaine fois où ça arrive

--- cesar.c
+++ cesar.c
@@ -9,41 +9,43 @@ int chercherOccurence(char lettre, char alphabet[]);
 char chercherLettrePosition(int position, char alphabet[]);
 void decalageAlphabet(int decalage, char alphabetADecaler[]);
 char* codageCesar(char* string, int decalage);
+char* deCodageCesar(char* string, int decalage);
 
 void main() {
 
     int decalage = 6;
     char* string = NULL;
     char* stringCodee;
+    char* stringDeCodee;
+
 
     printf("Code César avec un décalage de %d\n\n", decalage);
     string = saisieChaine();
-    printf("Avant codage : ");
-    afficherChaine(string);
     stringCodee = codageCesar(string, decalage);
     printf("Après codage : ");
     afficherChaine(stringCodee);
-
+    stringDeCodee = deCodageCesar(stringCodee, decalage);
+    printf("Après décodage : ");
+    afficherChaine(stringDeCodee);
 }
 
 char* saisieChaine(){
     char* string = NULL;
     size_t taille = 0;
     int nbCharLu = 0;
-    printf("Saisir le texte à coder :\n");
+    printf("Saisir le texte à coder : ");
     nbCharLu = getline(&string, &taille, stdin);
     return string;
 }
 
 void afficherChaine(const char* string){
-    printf("%s", string );
+    printf("%s\n", string );
 }
 
 void afficherAlphabet (char alphabet[]) {
     for(int i =0 ; i<26 ; i++){
         printf("%c", alphabet[i]);
     }
-    printf("\n");
 }
 
 void getAlphabet(char alphabet[]){
@@ -88,4 +90,18 @@ char* codageCesar(char* string, int decalage){
         stringCodee[i] = chercherLettrePosition(chercherOccurence(string[i], alphabet), alphabetDecale);
     }
     return stringCodee;
+}
+
+char* deCodageCesar(char* string, int decalage){
+    char alphabet[26];
+    char alphabetDecale[26];
+    char* stringDeCodee = string;
+
+    getAlphabet(alphabet);
+    decalageAlphabet(decalage, alphabetDecale);
+
+    for(int i= 0 ; i<strlen(string) ; i++){
+        stringDeCodee[i] = chercherLettrePosition(chercherOccurence(string[i], alphabetDecale), alphabet);
+    }
+    return stringDeCodee;
 }
